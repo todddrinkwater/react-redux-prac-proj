860e37dd346fdae7a77ddfa2c4d28f7f9023e427
todddrinkwater <todddrinkwaternz@gmail.com>
2017-06-12 21:12:06 +1200

    course actions and reducers set up

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,9 +1,37 @@
 import React, { PropTypes } from 'react'
 
-const CoursesPage = () => {
-  return (
-    <div>Courses</div>
-  )
+
+class CoursesPage extends React.Component {
+  constructor(props, context){
+    super(props, context)
+      this.state = {
+        course: { title: "" }
+      }
+      this.onTitleChange = this.onTitleChange.bind(this);
+      this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event){
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
+  onClickSave(){
+    alert(`Saving ${this.state.course.title}`)
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
+        <input type="button" onClick={this.onClickSave} value="Save" />
+      </div>
+    )
+  }
+
 }
 
 export default CoursesPage

ba7a06278abc747ee1c93122b38a9329221a774d
todddrinkwater <todddrinkwaternz@gmail.com>
2017-06-14 13:57:48 +1200

    setup basic redux flow

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,4 +1,6 @@
 import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+import * as courseActions from '../../actions/courseActions'
 
 
 class CoursesPage extends React.Component {
@@ -18,20 +20,35 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave(){
-    alert(`Saving ${this.state.course.title}`)
+    this.props.dispatch(courseActions.createCourse(this.state.course))
+  }
+
+  courseRow(course, index){
+    return <div key={index}>{course.title}</div>
   }
 
   render(){
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
         <input type="button" onClick={this.onClickSave} value="Save" />
       </div>
     )
   }
-
 }
 
-export default CoursesPage
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+}
+
+function mapStateToProps(state, ownProps){
+  return {
+    courses: state.courses
+  }
+}
+
+export default connect(mapStateToProps)(CoursesPage)
